42a8c2daedb962b83310887af5c61eacb4cf64d2
Michael Klein <Michaelklein1997@gmail.com>
2022-02-26 15:41:03 -0600

    updated list of technologies on portfolio

--- src/components/AboutMe/AboutMeContent.js
+++ src/components/AboutMe/AboutMeContent.js
@@ -1,6 +1,13 @@
 import classes from './AboutMeContent.module.css'
 import profilePic from '../../assets/images/profile-picture/profile-cut-out.png'
 import reactMarioBlock from '../../assets/images/technologies/react-mario-block.png'
+import css3MarioBlock from '../../assets/images/technologies/css3-logo-block.png'
+import googlePlatformMarioBlock from '../../assets/images/technologies/google-platform-logo-block.png'
+import mongodbMarioBlock from '../../assets/images/technologies/mongodb-logo-block.png'
+import mySQLMarioBlock from '../../assets/images/technologies/mySQL-logo-block.png'
+import nodejsMarioBlock from '../../assets/images/technologies/nodejs-mario-block.png'
+import javascriptMarioBlock from '../../assets/images/technologies/javascript-logo-block.png'
+
 
 import Card from '../UI/Card'
 
@@ -23,13 +30,13 @@ const AboutMeContent = () => {
                     <h3>These are a few technologies I use...</h3>
                 </div>
                 <div className={classes.technologies}>
+                    <img src={javascriptMarioBlock} alt="Mario block with React logo" />
                     <img src={reactMarioBlock} alt="Mario block with React logo" />
-                    <img src={reactMarioBlock} alt="Mario block with React logo" />
-                    <img src={reactMarioBlock} alt="Mario block with React logo" />
-                    <img src={reactMarioBlock} alt="Mario block with React logo" />
-                    <img src={reactMarioBlock} alt="Mario block with React logo" />
-                    <img src={reactMarioBlock} alt="Mario block with React logo" />
-                    <img src={reactMarioBlock} alt="Mario block with React logo" />
+                    <img src={mongodbMarioBlock} alt="Mario block with React logo" />
+                    <img src={nodejsMarioBlock} alt="Mario block with React logo" />
+                    <img src={mySQLMarioBlock} alt="Mario block with React logo" />
+                    <img src={css3MarioBlock} alt="Mario block with React logo" />
+                    <img src={googlePlatformMarioBlock} alt="Mario block with React logo" />
                 </div>
             </div>
 
